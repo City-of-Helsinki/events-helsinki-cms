d4077d2750b7b06e050578aa095232e04def874c
Sam Hosseini <git-email@hosseini.io>
2020-01-28 00:59:01 +0200

    add create_admin_superuser django management command

---
+++ application/management/commands/create_admin_superuser.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if settings.DEBUG is not True:
+            raise CommandError('This command can be run only in DEBUG mode')
+
+        admin_user_exists = User.objects.filter(username='admin').exists()
+        if admin_user_exists:
+            self.stdout.write('Superuser "admin" already exists')
+            return
+
+        User.objects.create_superuser('admin', 'admin@admin.com', 'coconut')
+        self.stdout.write('Superuser "admin" with "coconut" password created')
+
