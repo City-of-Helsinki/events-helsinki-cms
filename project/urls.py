f7af97161b06d807d8a2ec8df89b0efa0c562d1b
Sam Hosseini <git-email@hosseini.io>
2020-01-29 16:27:14 +0200

    add /api/ and /docs/ url paths to application

--- project/urls.py
+++ project/urls.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(title='API documentation', default_version='v1'),
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
+    path('api/', include('application.urls')),
+    path('docs/', schema_view.with_ui('swagger')),
 ]
