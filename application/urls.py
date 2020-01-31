0b243a62007cd7ad842cb980ed15ca4629353deb
Sam Hosseini <git-email@hosseini.io>
2020-01-31 15:53:51 +0200

    make readiness and healthz probes required by KuVa infra

--- application/urls.py
+++ application/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from application import views
+
+
+urlpatterns = [
+    path('healthz/', views.HealthCheck.as_view(), name='health-check'),
+    path('readiness/', views.ReadinessCheck.as_view(), name='readiness-check'),
+]
---
+++ application/views.py
@@ -0,0 +1,18 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class HealthCheck(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        return Response(status=status.HTTP_200_OK)
+
+
+class ReadinessCheck(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        return Response(status=status.HTTP_200_OK)
