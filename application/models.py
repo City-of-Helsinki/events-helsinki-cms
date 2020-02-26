bf2454abab2f863b918655958f9cdc3663b2283d
Sam Hosseini <git-email@hosseini.io>
2020-02-26 15:49:46 +0200

    add initial hierarchy for Helsinki Activities CMS needs

---
+++ application/models.py
@@ -0,0 +1,25 @@
+from wagtail.core.models import Page
+
+
+class HelsinkiActivities(Page):
+    parent_page_types = ['wagtailcore.Page']
+    subpage_typed = ['application.CollectionsGroup']
+
+    class Meta:
+        verbose_name = 'Helsinki Activities Service'
+
+
+class CollectionsGroup(Page):
+    parent_page_types = ['application.HelsinkiActivities']
+    subpage_typed = ['application.Collections']
+
+    class Meta:
+        verbose_name = 'Collection Group'
+
+
+class Collections(Page):
+    parent_page_types = ['application.CollectionsGroup']
+    subpage_typed = []
+
+    class Meta:
+        verbose_name = 'Collection'
