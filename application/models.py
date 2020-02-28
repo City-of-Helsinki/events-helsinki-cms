35c11477f9807efab90a3022a0aff14c8b8244e3
Sam Hosseini <git-email@hosseini.io>
2020-02-28 12:13:38 +0200

    add max_count to application.models
    There should be only one HelsinkiActivities instance and also
    one CollectionsGroup instance at a time in CMS so with max_count
    we are able to enfore that in the editor

--- application/models.py
+++ application/models.py
@@ -4,6 +4,7 @@ from wagtail.core.models import Page
 class HelsinkiActivities(Page):
     parent_page_types = ['wagtailcore.Page']
     subpage_typed = ['application.CollectionsGroup']
+    max_count = 1
 
     class Meta:
         verbose_name = 'Helsinki Activities Service'
@@ -12,6 +13,7 @@ class HelsinkiActivities(Page):
 class CollectionsGroup(Page):
     parent_page_types = ['application.HelsinkiActivities']
     subpage_typed = ['application.Collections']
+    max_count = 1
 
     class Meta:
         verbose_name = 'Collection Group'
