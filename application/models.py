6faea46c720c2b016ec0cac712ca1e161840fcc1
Sam Hosseini <git-email@hosseini.io>
2020-02-28 14:24:15 +0200

    add all fields required for a collection to Collections

--- application/models.py
+++ application/models.py
@@ -1,4 +1,8 @@
+from django.db import models
+from wagtail.core import blocks
 from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, StreamFieldPanel
 
 
 class HelsinkiActivities(Page):
@@ -22,6 +26,101 @@ class CollectionsGroup(Page):
 class Collections(Page):
     parent_page_types = ['application.CollectionsGroup']
     subpage_typed = []
+
+    # title comes from Page class itself
+    title_fi = models.CharField(max_length=255, null=True)
+    title_sv = models.CharField(max_length=255, null=True)
+
+    curated_events_title_en = models.CharField(max_length=255, null=True)
+    curated_events_title_fi = models.CharField(max_length=255, null=True)
+    curated_events_title_sv = models.CharField(max_length=255, null=True)
+
+    short_description_en = models.CharField(max_length=255, null=True)
+    short_description_fi = models.CharField(max_length=255, null=True)
+    short_description_sv = models.CharField(max_length=255, null=True)
+
+    description_en = models.TextField(null=True)
+    description_fi = models.TextField(null=True)
+    description_sv = models.TextField(null=True)
+
+    link_text_en = models.CharField(max_length=255, null=True)
+    link_text_fi = models.CharField(max_length=255, null=True)
+    link_text_sv = models.CharField(max_length=255, null=True)
+
+    link_url_en = models.URLField(max_length=200, null=True)
+    link_url_fi = models.URLField(max_length=200, null=True)
+    link_url_sv = models.URLField(max_length=200, null=True)
+
+    event_list_title_en = models.CharField(max_length=255, null=True)
+    event_list_title_fi = models.CharField(max_length=255, null=True)
+    event_list_title_sv = models.CharField(max_length=255, null=True)
+
+    event_list_query = models.URLField(max_length=200, null=True)
+
+    curated_events = StreamField([
+        ('event_link', blocks.URLBlock()),
+    ], null=True)
+
+    content_panels = [
+        MultiFieldPanel(
+            [
+                FieldPanel('title'),
+                FieldPanel('title_fi'),
+                FieldPanel('title_sv'),
+            ],
+            heading="Title",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('curated_events_title_en'),
+                FieldPanel('curated_events_title_fi'),
+                FieldPanel('curated_events_title_sv'),
+            ],
+            heading="Curated Events Title",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('short_description_en'),
+                FieldPanel('short_description_fi'),
+                FieldPanel('short_description_sv'),
+            ],
+            heading="Short Description",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('description_en'),
+                FieldPanel('description_fi'),
+                FieldPanel('description_sv'),
+            ],
+            heading="Description",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('link_text_en'),
+                FieldPanel('link_text_fi'),
+                FieldPanel('link_text_sv'),
+            ],
+            heading="Link Text",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('link_url_en'),
+                FieldPanel('link_url_fi'),
+                FieldPanel('link_url_sv'),
+            ],
+            heading="Link URL",
+        ),
+        MultiFieldPanel(
+            [
+                FieldPanel('event_list_title_en'),
+                FieldPanel('event_list_title_fi'),
+                FieldPanel('event_list_title_sv'),
+            ],
+            heading="Event List Title",
+        ),
+        FieldPanel('event_list_query'),
+        StreamFieldPanel('curated_events'),
+    ]
 
     class Meta:
         verbose_name = 'Collection'
