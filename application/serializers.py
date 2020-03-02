af5ade89998b1306b1097767e127d5e76bc6726d
Sam Hosseini <git-email@hosseini.io>
2020-03-02 15:57:41 +0200

    updated curated_events field in collections endpoint
    now curated_events field returns clean values

--- application/serializers.py
+++ application/serializers.py
@@ -4,6 +4,12 @@ from application import models
 
 
 class CollectionsSerializer(serializers.ModelSerializer):
+    curated_events = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Collections
         exclude = []
+
+    def get_curated_events(self, obj):
+        clean_curated_events = [curated_event.get('value') for curated_event in obj.curated_events.stream_data]
+        return clean_curated_events
