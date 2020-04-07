7c86a689218cfc811ee1193b800fec8e8f34da18
Sam Hosseini <git-email@hosseini.io>
2020-04-07 16:18:16 +0300

    update order of all fields in CMS for English to go last

--- application/models.py
+++ application/models.py
@@ -28,29 +28,29 @@ class LandingPage(Page):
     subpage_typed = []
     max_count = 1
 
-    # title comes from Page class itself
     title_fi = models.CharField(max_length=255, null=True)
     title_sv = models.CharField(max_length=255, null=True)
+    # title comes from Page class itself
 
-    description_en = models.TextField(null=True)
     description_fi = models.TextField(null=True)
     description_sv = models.TextField(null=True)
+    description_en = models.TextField(null=True)
 
-    button_text_en = models.CharField(max_length=255, null=True)
     button_text_fi = models.CharField(max_length=255, null=True)
     button_text_sv = models.CharField(max_length=255, null=True)
+    button_text_en = models.CharField(max_length=255, null=True)
 
-    button_url_en = models.URLField(max_length=500, null=True)
     button_url_fi = models.URLField(max_length=500, null=True)
     button_url_sv = models.URLField(max_length=500, null=True)
+    button_url_en = models.URLField(max_length=500, null=True)
 
-    meta_information_en = models.TextField(null=True)
     meta_information_fi = models.TextField(null=True)
     meta_information_sv = models.TextField(null=True)
+    meta_information_en = models.TextField(null=True)
 
-    page_title_en = models.CharField(max_length=255, null=True)
     page_title_fi = models.CharField(max_length=255, null=True)
     page_title_sv = models.CharField(max_length=255, null=True)
+    page_title_en = models.CharField(max_length=255, null=True)
 
     class Meta:
         verbose_name = 'Landing Page'
@@ -58,54 +58,54 @@ class LandingPage(Page):
     content_panels = [
         MultiFieldPanel(
             [
-                FieldPanel('title'),
                 FieldPanel('title_fi'),
                 FieldPanel('title_sv'),
+                FieldPanel('title'),
             ],
             heading="Title",
             help_text='Help text number 1',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('description_en'),
                 FieldPanel('description_fi'),
                 FieldPanel('description_sv'),
+                FieldPanel('description_en'),
             ],
             heading="Description",
             help_text='Help text number 2',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('button_text_en'),
                 FieldPanel('button_text_fi'),
                 FieldPanel('button_text_sv'),
+                FieldPanel('button_text_en'),
             ],
             heading="Button Text",
             help_text='Help text number 3',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('button_url_en'),
                 FieldPanel('button_url_fi'),
                 FieldPanel('button_url_sv'),
+                FieldPanel('button_url_en'),
             ],
             heading="Button Url",
             help_text='Help text number 4',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('meta_information_en'),
                 FieldPanel('meta_information_fi'),
                 FieldPanel('meta_information_sv'),
+                FieldPanel('meta_information_en'),
             ],
             heading="Meta Information",
             help_text='Help text number 5',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('page_title_en'),
                 FieldPanel('page_title_fi'),
                 FieldPanel('page_title_sv'),
+                FieldPanel('page_title_en'),
             ],
             heading="Page Title",
             help_text='Help text number 6',
@@ -125,37 +125,37 @@ class Collections(Page):
 
     box_color = models.CharField(max_length=255, choices=color_choices, null=True)
 
-    # title comes from Page class itself
     title_fi = models.CharField(max_length=255, null=True, blank=True)
     title_sv = models.CharField(max_length=255, null=True, blank=True)
+    # title comes from Page class itself
 
-    curated_events_title_en = models.CharField(max_length=255, null=True, blank=True)
     curated_events_title_fi = models.CharField(max_length=255, null=True, blank=True)
     curated_events_title_sv = models.CharField(max_length=255, null=True, blank=True)
+    curated_events_title_en = models.CharField(max_length=255, null=True, blank=True)
 
-    subtitles_en = models.CharField(max_length=255, null=True, blank=True)
     subtitles_fi = models.CharField(max_length=255, null=True, blank=True)
     subtitles_sv = models.CharField(max_length=255, null=True, blank=True)
+    subtitles_en = models.CharField(max_length=255, null=True, blank=True)
 
-    description_en = models.TextField(null=True, blank=True)
     description_fi = models.TextField(null=True, blank=True)
     description_sv = models.TextField(null=True, blank=True)
+    description_en = models.TextField(null=True, blank=True)
 
-    social_media_description_en = models.TextField(null=True, blank=True)
     social_media_description_fi = models.TextField(null=True, blank=True)
     social_media_description_sv = models.TextField(null=True, blank=True)
+    social_media_description_en = models.TextField(null=True, blank=True)
 
-    link_text_en = models.CharField(max_length=255, null=True, blank=True)
     link_text_fi = models.CharField(max_length=255, null=True, blank=True)
     link_text_sv = models.CharField(max_length=255, null=True, blank=True)
+    link_text_en = models.CharField(max_length=255, null=True, blank=True)
 
-    link_url_en = models.URLField(max_length=500, null=True, blank=True)
     link_url_fi = models.URLField(max_length=500, null=True, blank=True)
     link_url_sv = models.URLField(max_length=500, null=True, blank=True)
+    link_url_en = models.URLField(max_length=500, null=True, blank=True)
 
-    event_list_title_en = models.CharField(max_length=255, null=True, blank=True)
     event_list_title_fi = models.CharField(max_length=255, null=True, blank=True)
     event_list_title_sv = models.CharField(max_length=255, null=True, blank=True)
+    event_list_title_en = models.CharField(max_length=255, null=True, blank=True)
 
     event_list_query = models.URLField(max_length=500, null=True)
 
@@ -173,72 +173,72 @@ class Collections(Page):
         ),
         MultiFieldPanel(
             [
-                FieldPanel('title'),
                 FieldPanel('title_fi'),
                 FieldPanel('title_sv'),
+                FieldPanel('title'),
             ],
             heading="Title",
             help_text='Help text number 1',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('curated_events_title_en'),
                 FieldPanel('curated_events_title_fi'),
                 FieldPanel('curated_events_title_sv'),
+                FieldPanel('curated_events_title_en'),
             ],
             heading="Curated Events Title",
             help_text='Help text number 2',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('subtitles_en'),
                 FieldPanel('subtitles_fi'),
                 FieldPanel('subtitles_sv'),
+                FieldPanel('subtitles_en'),
             ],
             heading="Subtitles",
             help_text='Help text number 3',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('description_en'),
                 FieldPanel('description_fi'),
                 FieldPanel('description_sv'),
+                FieldPanel('description_en'),
             ],
             heading="Description",
             help_text='Help text number 4',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('social_media_description_en'),
                 FieldPanel('social_media_description_fi'),
                 FieldPanel('social_media_description_sv'),
+                FieldPanel('social_media_description_en'),
             ],
             heading="Social Media Description",
             help_text='Help text number 5',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('link_text_en'),
                 FieldPanel('link_text_fi'),
                 FieldPanel('link_text_sv'),
+                FieldPanel('link_text_en'),
             ],
             heading="Link Text",
             help_text='Help text number 6',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('link_url_en'),
                 FieldPanel('link_url_fi'),
                 FieldPanel('link_url_sv'),
+                FieldPanel('link_url_en'),
             ],
             heading="Link URL",
             help_text='Help text number 7',
         ),
         MultiFieldPanel(
             [
-                FieldPanel('event_list_title_en'),
                 FieldPanel('event_list_title_fi'),
                 FieldPanel('event_list_title_sv'),
+                FieldPanel('event_list_title_en'),
             ],
             heading="Event List Title",
             help_text='Help text number 8',
