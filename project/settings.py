b166bd380ec2fb36740d6b707ee290b425ffdfba
Sam Hosseini <git-email@hosseini.io>
2020-04-22 16:12:40 +0300

    Revert "add all configs necessary to handle media files in S3"
    This reverts commit abc585eca558be3d3894ecf8a67d86dfab2e3425.
    
    This is because we have support only for Google Cloud and Azure object
    storages in our infra, not S3.

--- project/settings.py
+++ project/settings.py
@@ -145,19 +145,6 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static-files')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media-files')
 
-# Django Storages configs for media files
-if DEBUG is True:
-    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
-else:
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-
-AWS_DEFAULT_ACL = None
-AWS_S3_FILE_OVERWRITE = False
-
 # CORS settings
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',  # local development environment of frontend application
