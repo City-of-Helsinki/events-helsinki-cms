c3ea1a42a4ffdac4e10f29220cb434e611f6c623
Sam Hosseini <git-email@hosseini.io>
2020-07-03 22:01:07 +0300

    overwrite first Finnish translation for a Wagtail message
    The Wagtail message of:
    
    "The page could not be saved due to validation errors"
    
    Is not understandable enough for our content creators. This had to be
    overwritten to:
    
    "Syöttämisässi tiedoissa on virhe"
    
    For this, application/wagtail_messages.py has been added with
    instructions for future overwriting of messages as well.

---
+++ application/wagtail_messages.py
@@ -0,0 +1,13 @@
+from django.utils.translation import gettext
+
+'''
+https://github.com/wagtail/wagtail/blob/master/wagtail/admin/locale/fi/LC_MESSAGES/django.po
+
+To overwrite what Wagtail shows for default translated Finnish texts:
+
+1. We must *mark* the message we would like to overwrite in this module using gettext
+2. We must *update* the Finnish locale file with the message we marked using: django-admin makemessages --locale fi
+3. We must *provide* our own desired translation in the Finnish locale file in locale/fi/LC_MESSAGES/django.po
+4. We must *compile* the Finnish locale file using: django-admin compilemessages
+'''
+gettext("The page could not be saved due to validation errors")
