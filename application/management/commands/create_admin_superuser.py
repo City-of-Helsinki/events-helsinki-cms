bab63415dd505b4059a5fb073d3144018030ec43
Sam Hosseini <git-email@hosseini.io>
2020-08-20 12:22:20 +0300

    udpate admin_superuser django command to use SUPERUSER_PASSWORD env var

--- application/management/commands/create_admin_superuser.py
+++ application/management/commands/create_admin_superuser.py
@@ -1,17 +1,20 @@
-from django.conf import settings
+import os
+
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if settings.DEBUG is not True:
-            raise CommandError('This command can be run only in DEBUG mode')
+        SUPERUSER_PASSWORD = os.getenv('SUPERUSER_PASSWORD')
+
+        if not SUPERUSER_PASSWORD:
+            raise CommandError('SUPERUSER_PASSWORD env variable is not set')
 
         admin_user_exists = User.objects.filter(username='admin').exists()
         if admin_user_exists:
             self.stdout.write('Superuser "admin" already exists')
             return
 
-        User.objects.create_superuser('admin', 'admin@admin.com', 'coconut')
-        self.stdout.write('Superuser "admin" with "coconut" password created')
+        User.objects.create_superuser('admin', 'admin@admin.com', SUPERUSER_PASSWORD)
+        self.stdout.write('Superuser "admin" successfully created')
